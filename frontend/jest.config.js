d8a99200939cb88775feef8c21f604a1b73990e2
Luis Arpasi <luiggi@Luiss-MBP.lan>
2019-01-14 23:06:24 -0500

    Initial files to start

---
+++ frontend/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	verbose: true,
+	setupTestFrameworkScriptFile: '<rootDir>/jest.setup.js',
+	testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+	transform: {
+		'\\.(gql|graphql)$': 'jest-transform-graphql',
+		'.*': 'babel-jest',
+		'^.+\\.js?$': 'babel-jest'
+	}
+}
