f90be3db1f9345fc7d7057a0b0d418a88b7796a5
Luis Arpasi <luiggi@Luiss-MBP.lan>
2019-01-27 23:53:49 -0500

    First Schema, mutation and query

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,18 @@
-const mutations = {};
+const mutations = {
+	async createItem(parent, args, ctx, info) {
+		// TODO: Check if they are logged in
 
-module.exports = mutations;
+		const item = await ctx.db.mutation.createItem(
+			{
+				data: {
+					...args
+				}
+			},
+			info
+		)
+
+		return item
+	}
+}
+
+module.exports = mutations
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,13 @@
-const Query = {};
+// If query is the same as the generated one in prisma.graphql
+// then we can forward it directly (no filter, auth or any other logic)
+const { forwardTo } = require('prisma-binding')
 
-module.exports = Query;
+const Query = {
+	items: forwardTo('db')
+	// async items(parent, args, ctx, info) {
+	// 	const items = await ctx.db.query.items()
+	// 	return items
+	// }
+}
+
+module.exports = Query
