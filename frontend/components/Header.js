ef9cabfea4ce513492050037ac8acd685878efa3
Luis Arpasi <luiggi@Luiss-MBP.lan>
2019-02-13 22:28:09 -0500

    AutoComplete Part I

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -5,6 +5,7 @@ import styled from 'styled-components'
 import Router from 'next/router'
 import NProgress from 'nprogress'
 import Cart from './Cart'
+import AutoComplete from './Search'
 
 Router.onRouteChangeStart = () => {
 	NProgress.start()
@@ -67,7 +68,7 @@ const Header = () => {
 				<Nav />
 			</div>
 			<div className='sub-bar'>
-				<p>Search</p>
+				<AutoComplete />
 			</div>
 			<Cart />
 		</StyledHeader>
---
+++ frontend/components/Search.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+
+import { DropDown, DropDownItem, SearchStyles } from './styles/DropDown'
+import gql from 'graphql-tag'
+import { ApolloConsumer } from 'react-apollo'
+import debounce from 'lodash.debounce'
+
+const SEARCH_ITEMS_QUERY = gql`
+	query SEARCH_ITEMS_QUERY($searchTerm: String!) {
+		items(
+			where: {
+				OR: [
+					{ title_contains: $searchTerm }
+					{ description_contains: $searchTerm }
+				]
+			}
+		) {
+			id
+			image
+			title
+		}
+	}
+`
+
+class AutoComplete extends Component {
+	state = {
+		items: [],
+		loading: false
+	}
+
+	onChange = debounce(async (e, client) => {
+		this.setState({ loading: true })
+
+		// Manually query apollo client
+		const res = await client.query({
+			query: SEARCH_ITEMS_QUERY,
+			variables: { searchTerm: e.target.value }
+		})
+
+		this.setState({ items: res.data.items, loading: false })
+	}, 350)
+
+	render() {
+		return (
+			<SearchStyles>
+				<div>
+					<ApolloConsumer>
+						{client => (
+							<input
+								type='search'
+								onChange={e => {
+									e.persist()
+									this.onChange(e, client)
+								}}
+							/>
+						)}
+					</ApolloConsumer>
+					<DropDown>
+						{this.state.items.map(item => (
+							<DropDownItem key={item.id}>
+								<img src={item.image} alt={item.title} width='50' />
+								{item.title}
+							</DropDownItem>
+						))}
+					</DropDown>
+				</div>
+			</SearchStyles>
+		)
+	}
+}
+
+export default AutoComplete
